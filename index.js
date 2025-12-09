3c312c5e0e028414c1c4b69b5b912997ddf17dc4
Chetan <naikc8468@gmail.com>
2025-12-09 13:13:24 +0000

    Solution

---
+++ index.js
@@ -0,0 +1,29 @@
+const Button = props => {
+  //  Write your code here.
+  const {className, buttonText} = props
+
+  return
+  ;<button className={`button ${className}`}>{buttonText}</button>
+}
+
+const element = (
+  //  Write your code here.
+  <div className='Social-Buttons-bg-container'>
+    <h1 className='heading'>Social Buttons</h1>
+    <div className='button-container'>
+      <Button className='like-button' buttonText='Like' />
+      <Button className='comment-button' buttonText='Comment' />
+      <Button className='share-button' buttonText='Share' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
+
+
+
+  git config --global user.email naikc8468@gmail.com
+    git config --global user.name Chetan
+
+     git remote add origin https://github.com/Chetannaik-9535/Social-Buttons.git
+     
