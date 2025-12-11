67dd293fb400e5fe7ae667a5053f7f46590e46e4
Chetan <naikc8468@gmail.com>
2025-12-11 13:19:42 +0000

    Solution

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ ReactDOM.render(element, document.getElementById('root'))
 
 
 
-  git config --global user.email naikc8468@gmail.com
+ {/* git config --global user.email naikc8468@gmail.com
     git config --global user.name Chetan
 
      git remote add origin https://github.com/Chetannaik-9535/Social-Buttons.git
-     
+     chetan@9535
+
+     git commit -m "Social buttons App" */}
